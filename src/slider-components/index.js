21450961060f1ce6d85fd98fb938a31f1482004a
“Daniel” <daniel.fabian13x@gmail.com>
2022-11-07 23:36:54 +0100

    Fix it import and updated bundle

--- src/slider-components/index.js
+++ src/slider-components/index.js
@@ -1,9 +1,7 @@
 import { createWebComponent } from './libs/createWebComponent.js'
 import { SliderComponent } from './components/SliderComponent/SliderComponent.js'
-import { SliderCyclicComponent } from './components/SliderCyclicComponent/SliderCyclicComponent.js'
 
 export {
   createWebComponent,
-  SliderComponent,
-  SliderCyclicComponent
+  SliderComponent
 }
